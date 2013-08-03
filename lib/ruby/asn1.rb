b40e77e66e83f15ade056b0c3223501ba980e2c8
srcdaniel <srcdaniel@gmail.com>
2013-08-03 07:47:07 -0400

    Recreate using sow

---
+++ lib/ruby/asn1.rb
@@ -0,0 +1,5 @@
+module Ruby; end
+
+class Ruby::Asn1
+  VERSION = "1.0.0"
+end
---
+++ test/ruby/test_asn1.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require "ruby/asn1"
+
+module TestRuby; end
+
+class TestRuby::TestAsn1 < MiniTest::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
